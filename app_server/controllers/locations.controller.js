45afa921d87627d4241480c8414fa1c749c7c6b6
phongngo <phong.trieungo@hotmail.com>
2020-07-06 23:49:10 +0700

    Location detail page with static data

--- app_server/controllers/locations.controller.js
+++ app_server/controllers/locations.controller.js
@@ -1,5 +1,5 @@
 const index = (req, res) => res.render('location-list', { title: 'Location page'});
-const detail = (req, res) => res.render('index', { title: 'Location Detail page'});
+const detail = (req, res) => res.render('location-info', { title: 'Location Detail page'});
 const addReview = (req, res) => res.render('index', { title: 'Add Review page'});
 
 module.exports = {
