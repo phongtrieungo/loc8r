dc39b54a285221b71224643d855f939394ab8d2a
Phong Ngo <phong.trieungo@hotmail.com>
2020-07-06 15:23:06 +0700

    Adding route and controller

---
+++ app_server/controllers/about.controller.js
@@ -0,0 +1,5 @@
+const index = (req, res) => res.render('index', { title: 'About page'});
+
+module.exports = {
+    index
+};
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const mainController = require('../controllers/main');
+const locationController = require('../controllers/locations.controller');
+const aboutController = require('../controllers/about.controller');
 
 /* GET home page. */
-router.get('/', mainController.index);
+router.get('/locations/review/new', locationController.addReview);
+router.get('/locations', locationController.detail);
+router.get('/about', aboutController.index);
+router.get('/', locationController.index)
 
 module.exports = router;
